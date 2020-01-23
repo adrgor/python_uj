d4a8d422ad382bbda7c61eeac1e9bbec43d3aa81
adrgor <56635662+adrgor@users.noreply.github.com>
2020-01-23 16:31:38 +0100

    Add files via upload

---
+++ Projekt-Graham-ver2/test.py
@@ -0,0 +1,26 @@
+import unittest
+from point import Point
+from helpFun import find_hull
+
+class TestFractions(unittest.TestCase):
+
+	def setUp(self):
+		self.list_test_1 = [Point(0,0), Point(0,2), Point(0,4),
+					Point(2,0), Point(2,2), Point(2,4),
+					Point(4,0), Point(4,2), Point(4,4)]
+		self.list_test_2 = [Point(0,0), Point(2,0), Point(4,0), Point(6,0)]
+		self.list_test_3 = [Point(2,0), Point(1,0), Point(3,1), Point(2,2), Point(4,3), Point(4,2), Point(3,2), Point(1,4)]
+
+		self.result_list_1 = [self.list_test_1[0], self.list_test_1[6], self.list_test_1[8], self.list_test_1[2]]
+		self.result_list_2 = [self.list_test_2[0], self.list_test_2[3]]
+		self.result_list_3 = [self.list_test_3[1], self.list_test_3[0],self.list_test_3[5], self.list_test_3[4], self.list_test_3[7]]
+
+	def test_graham(self): 
+		self.assertEqual(find_hull(self.list_test_1), self.result_list_1)
+		self.assertEqual(find_hull(self.list_test_2), self.result_list_2)
+		self.assertEqual(find_hull(self.list_test_3), self.result_list_3)
+
+    
+
+if __name__ == '__main__':
+	unittest.main()     # uruchamia wszystkie testy
